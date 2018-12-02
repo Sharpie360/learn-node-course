f9b5860b40824fd6bd195e02c02fca4241e8bdd0
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-01 19:16:52 -0600

    updated - es6 promise, async await, flash messages

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,11 +1,36 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
-  res.render('index')
-}
+  req.flash('error', 'Something went wrong')
+  req.flash('info', 'Some knowledge')
+  req.flash('warning', 'Something went somewhere')
+  req.flash('success', 'Something went right')
+  res.render('index');
+};
 
 exports.addStore = (req, res) => {
-  res.render('editStore', { title: 'Add Store'})
-}
+  res.render('editStore', { title: 'Add Store' });
+};
 
-exports.createStore = (req, res) => {
-  res.json(req.body)
-}
+// exports.createStore = (req, res) => {
+//   const store = new Store(req.body);
+//   store
+//     .save()
+//     .then(store => {
+//       return Store.find();
+//     })
+//     .then(stores => {
+//       res.render('storeList', { stores: stores });
+//     })
+//     .catch(err => {
+//       throw Error(err);
+//     });
+// };
+
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  req.flash('success', `Successfully Created ${store.name}, Care to leave a review?`)
+  res.redirect('/');
+};
